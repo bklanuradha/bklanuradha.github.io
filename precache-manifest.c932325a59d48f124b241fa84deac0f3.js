4878f800991b81eb9c78f00093825bcd4334ccfe
bklanuradha <92729@fhss.sjp.ac.lk>
2021-06-11 13:04:53 +0000

    Updates

---
+++ precache-manifest.c932325a59d48f124b241fa84deac0f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4310a16fa02067c0922e9f5f7abb85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b745c2c6b88b761e4c82",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b745c2c6b88b761e4c82",
+    "url": "/static/js/main.920e5a43.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "77f38bf6a812e7771da9fdbaef6bd411",
+    "url": "/static/media/anuradha.77f38bf6.jpeg"
+  },
+  {
+    "revision": "24f41b8791965d39ef9be11ee2c050c5",
+    "url": "/static/media/resume.24f41b87.pdf"
+  }
+]);
